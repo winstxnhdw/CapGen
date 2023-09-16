f549d30de83150a7f9d8514ff08e9ea1e8488d88
winstxnhdw <winstxnhdw@gmail.com>
2023-09-17 06:03:24 +0800

    feat: add unit test

---
+++ tests/test_transcribe.py
@@ -0,0 +1,22 @@
+# pylint: disable=missing-function-docstring,redefined-outer-name
+
+from fastapi.testclient import TestClient
+from pytest import fixture
+
+from server import Server
+
+
+@fixture()
+def client():
+    return TestClient(next(Server.initialise()))
+
+
+def test_transcribe(client: TestClient):
+    with open('tests/test.mp3', 'rb') as file:
+        response = client.post(
+            '/v1/transcribe',
+            files={ 'request': file },
+            params={ 'transcription_type': 'srt' }
+        ).json()
+
+    assert response['result'] == '1\n00:00:00,000 --> 00:00:02,000\nHello there, my name is Bella.'
