8c202dcfb32fb006fd52738b01be23cbae579e63
winstxnhdw <winstxnhdw@gmail.com>
2023-09-18 02:33:35 +0800

    chore: list specific `compute_type`

--- capgen/types/options.py
+++ capgen/types/options.py
@@ -1,4 +1,4 @@
-from typing import TypedDict
+from typing import Literal, TypedDict
 
 
 class TranscriberOptions(TypedDict):
@@ -14,5 +14,16 @@ class TranscriberOptions(TypedDict):
     num_workers (int) : the number of workers
     """
     model_size_or_path: str
-    compute_type: str
     num_workers: int
+    compute_type: Literal[
+        'default',
+        'auto',
+        'int8',
+        'int8_float32',
+        'int8_float16',
+        'int8_bfloat16',
+        'int16'
+        'float16',
+        'bfloat16',
+        'float32',
+    ]
