6bd7f602316a1c0b9134563f7a33ba7b94f226bd
Winston H. <56998716+winstxnhdw@users.noreply.github.com>
2025-07-20 19:07:59 +0100

    docs: remove docstrings

--- server/src/schemas/v1/transcribed.py
+++ server/src/schemas/v1/transcribed.py
@@ -4,14 +4,4 @@ from msgspec import Meta, Struct
 
 
 class Transcribed(Struct):
-    """
-    Summary
-    -------
-    the transcribed schema
-
-    Attributes
-    ----------
-    result (str) : the transcribed text in the chosen caption file format
-    """
-
     result: Annotated[str, Meta(examples=['1\n00:00:00,000 --> 00:00:02,000\nHello world.'])]
